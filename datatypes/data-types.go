5494719570ec56f01230326a0d334df2c577dd51
Himanshu Gautam <himanshu@aplimasolutions.com>
2020-04-25 12:42:50 -0700

    add data types

---
+++ datatypes/data-types.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    fmt.Println("go" + "lang")
+
+    fmt.Println("1+1 =", 1+1)
+    fmt.Println("15.0/6.0 =", 15.0/6.0)
+
+    fmt.Println(true && false)
+    fmt.Println(true || false)
+    fmt.Println(!true)
+}
